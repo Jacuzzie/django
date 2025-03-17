d151c7550ac598f70af42a499d8e0d280f97f0b8
Jaryd <jaryd.osiecki@icloud.com>
2025-03-17 14:47:38 +0000

    feat(main): add tests for home view

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class TestHome(TestCase):
+    def test_status(self):
+        response = self.client.get(reverse('main:home'))
+        self.assertEqual(response, 200)
